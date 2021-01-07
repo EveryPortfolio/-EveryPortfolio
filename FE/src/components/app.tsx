6acd57e16dce6ed385816acb9a3d5573c91a402f
JinGu <xortm854@naver.com>
2021-01-08 00:40:33 +0900

    [FE]feat: 상위 컴포넌트 App.tsx 구현
    상위 컴포넌트를 만들기 위해 app.tsx를 생성하였습니다.

---
+++ FE/src/components/app.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Profile from './Test/Profile/Profile';
+
+const App = (): JSX.Element => {
+  return <Profile username='TwentyFiveSeven' name='Kang' />;
+};
+
+export default App;
