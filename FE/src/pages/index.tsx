0b1df8e0db1d9686bcb1b894e22b93d2f2c362c0
KimHyunSeok <sfsfkj@gmail.com>
2021-01-08 18:19:52 +0900

    [FE]add: pages폴더 생성
    빌드 테스트를 해보기 위한 pages폴더 생성
    pages라는 이름은 변경되면 안된다.
    이 페이지 안의 파일, 폴더가 곧 라우팅될 주소를 의미하게 된다.

---
+++ FE/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const Index: React.FunctionComponent = () => {
+  return (
+    <div>
+      <p>here is index page</p>
+    </div>
+  );
+};
+
+export default Index;
