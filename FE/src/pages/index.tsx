97b0b594578b7461b2bce0519dc81523498d9ac9
JinGu <xortm854@naver.com>
2021-01-12 23:34:18 +0900

    [FE]feat: 글로벌 스타일 적용
    글로벌 스타일을 적용하였습니다.

--- FE/src/pages/index.tsx
+++ FE/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
-import LoginComponent from '../components/LoginComponent';
+import LoginComponent from '../components/LoginComponent/LoginComponent';
+import { GlobalStyle } from '../styles/GlobalStyle';
 
 const Index: React.FunctionComponent = () => {
   return (
     <div>
+      <GlobalStyle />
       <LoginComponent />
     </div>
   );
