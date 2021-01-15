757e3c8c46aa4d4317ed94c47f9c709a7ffe09a4
JinGu <xortm854@naver.com>
2021-01-15 18:36:11 +0900

    [FE]feat: 최상단 컴포넌트에 Store 적용 -#30
    사용자의 상태관리 및 전체적인 상태를 저장하기위한 store를 최상단
    컴포넌트에 선언해줬습니다.

--- FE/src/pages/index.tsx
+++ FE/src/pages/index.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
+import { Provider } from 'react-redux';
 import LoginComponent from '../components/LoginComponent/LoginComponent';
 import { GlobalStyle } from '../styles/GlobalStyle';
+import store from '../store/store';
 
 const Index: React.FunctionComponent = () => {
   return (
     <div>
-      <GlobalStyle />
-      <LoginComponent />
+      <Provider store={store}>
+        <GlobalStyle />
+        <LoginComponent />
+      </Provider>
     </div>
   );
 };
