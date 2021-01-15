cb4e6bd3e14ddb2428b2216dbeeb4a1bd32a9615
JinGu <xortm854@naver.com>
2021-01-15 18:34:13 +0900

    [FE]feat: user 상태관리를 위한 모듈 구현 -#30
    사용자의 로그인 유무를 관리하기 위한 Action, Action Function, Reducer를
    포함하고있는 모듈을 구현하였습니다.

---
+++ FE/src/store/modules/user.ts
@@ -0,0 +1,37 @@
+import { loginAPI } from '../../api/index';
+
+const LOGIN_SUCCESS = 'user/LOGINS';
+
+export const requestLogin = (params: any) => {
+  /**
+   * @TODO
+   * API 맞춰서 작성하기
+   * const data = loginAPI(params);
+   */
+  const data = new Promise((resolve) => {
+    return resolve('check');
+  });
+
+  return {
+    type: LOGIN_SUCCESS,
+    payload: data,
+  };
+};
+
+const initialState = {
+  successLogin: '',
+};
+
+export default function user(state = initialState, action: any): any {
+  switch (action.type) {
+    case `${LOGIN_SUCCESS}_FULFILLED`:
+      console.log('LOGIN_SUCCESS Type', action);
+      return {
+        ...state,
+        successLogin: action.payload,
+      };
+    default:
+      console.log('Default Type:', action);
+      return state;
+  }
+}
