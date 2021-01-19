ca12a0151d54878dff28cf6710307f24bdd8f554
JinGu <xortm854@naver.com>
2021-01-19 23:21:29 +0900

    [FE]refactor: 브라우저 도구 관련 미들웨어 추가 -#30
    브라우저에서 Redux 관련 탭이나 redux 상태값을 확인하기 쉬운 도구들을
    위한 미들웨어를 적용하였습니다.

--- FE/src/store/store.ts
+++ FE/src/store/store.ts
@@ -1,8 +1,14 @@
 import { applyMiddleware, createStore } from 'redux';
 import promiseMiddlerware from 'redux-promise-middleware';
 import reduxThunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
 import reducer from './modules';
 
-const createStoreWidthMiddleware = applyMiddleware(promiseMiddlerware, reduxThunk)(createStore);
+// const createStoreWidthMiddleware = applyMiddleware(promiseMiddlerware, reduxThunk)(createStore);
+const createStoreWidthMiddleware = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(logger, promiseMiddlerware, reduxThunk)),
+); // 여러개의 미들웨어를 적용 할 수 있습니다.
 
-export default createStoreWidthMiddleware(reducer);
+export default createStoreWidthMiddleware;
