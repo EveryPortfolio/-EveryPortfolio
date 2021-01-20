1a16f6ae2a5006452e7f90470e51acc1b9993e25
JinGu <xortm854@naver.com>
2021-01-21 00:03:00 +0900

    [FE]add: Redux-Persist Store 적용 -#30
    redux-persist의 persistGate를 사용하여 persist를 적용해줬습니다.

--- FE/src/pages/index.tsx
+++ FE/src/pages/index.tsx
@@ -1,15 +1,18 @@
 import * as React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import LoginComponent from '../components/LoginComponent/LoginComponent';
 import { GlobalStyle } from '../styles/GlobalStyle';
-import store from '../store/store';
+import { store, persistor } from '../store/store';
 
 const Index: React.FunctionComponent = () => {
   return (
     <div>
       <Provider store={store}>
-        <GlobalStyle />
-        <LoginComponent />
+        <PersistGate persistor={persistor}>
+          <GlobalStyle />
+          <LoginComponent />
+        </PersistGate>
       </Provider>
     </div>
   );
