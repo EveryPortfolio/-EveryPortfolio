98dd1cf807a3c42c67af05d8097d7c4299959535
JinGu <xortm854@naver.com>
2021-01-20 23:48:18 +0900

    [FE]refactor: API URL 변경-#30
    기존에 /api/user/signin 이었던 URL을 /api/user/login으로 수정하였습니다.

--- FE/src/api/index.ts
+++ FE/src/api/index.ts
@@ -2,5 +2,5 @@ import { Axios } from './config';
 
 export const loginAPI = (params: any): Promise<string> => {
   console.log(params);
-  return Axios('post', '/api/user/signin', params);
+  return Axios('post', '/api/user/login', params);
 };
