95a0f2ceb4b772e7cf7ca7ecd29201afe9cd7557
JinGu <xortm854@naver.com>
2021-01-20 23:49:49 +0900

    [FE]feat: Redux Container 생성 - #30
    Redux의 경우 presentation component와 Container Component를 통해서
    껍데기인 컴포넌트와 상태를 갖고있는 컴포넌트를 구분하여 구현하기 때문에
    이것을 적용하였습니다.

---
+++ FE/src/container/LoginContainer/RightContainer.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useDispatch } from 'react-redux';
+import { RightComponent } from '../../components/LoginComponent/RightComponent/RightComponent';
+import { requestLogin } from '../../store/modules/user';
+
+export const RightContainer = (): JSX.Element => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  console.log('render Right');
+
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('change Email');
+    setEmail(e.target.value);
+  };
+
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('change password');
+    setPassword(e.target.value);
+  };
+
+  const onClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    console.log('click button');
+    const params = { id: email, password };
+    dispatch(requestLogin(params));
+    setEmail('');
+    setPassword('');
+    // router.push('/');
+  };
+
+  return (
+    <RightComponent
+      email={email}
+      onChangeEmail={onChangeEmail}
+      password={password}
+      onChangePassword={onChangePassword}
+      onClick={onClick}
+    />
+  );
+};
