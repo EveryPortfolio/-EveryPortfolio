f01aa58ac124ff5ade655fb47f2a81e6bb9f1af1
Yeong Jun Joo <juyj7282@gmail.com>
2021-01-22 15:49:31 +0900

    [BE] add: db parameters 수정 - #68
    db paramter가 test중에 인식을 안하길래 @Param annotation을 붙여서 확실히
    구분하도록 만듬

--- BE/user/src/main/java/com/everyportfolio/user/mapper/UserMapper.java
+++ BE/user/src/main/java/com/everyportfolio/user/mapper/UserMapper.java
@@ -2,12 +2,13 @@ package com.everyportfolio.user.mapper;
 
 import com.everyportfolio.user.model.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface UserMapper {
-    User selectUserById(String id);
+    User selectUserById(@Param("id") String id);
     void insertUser(User user);
-    void updateUserTokenById(String id, String token);
-    String selectUserTokenById(String id);
-    void deleteUserById(String id);
+    void updateUserTokenById(@Param("id") String id, @Param("token") String token);
+    String selectUserTokenById(@Param("id") String id);
+    void deleteUserById(@Param("id") String id);
 }
