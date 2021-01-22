c14020559a0d4f153f8c3b5d56d0dfa2e883caf1
Yeong Jun Joo <juyj7282@gmail.com>
2021-01-22 15:48:08 +0900

    [BE] add: loginDTO 수정 - #56
    login 테스트하기위해서 loginDTO에 ToString annotation 추가

--- BE/user/src/main/java/com/everyportfolio/user/DTO/LoginDTO.java
+++ BE/user/src/main/java/com/everyportfolio/user/DTO/LoginDTO.java
@@ -1,14 +1,12 @@
 package com.everyportfolio.user.DTO;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class LoginDTO {
     private String id;
     private String password;
