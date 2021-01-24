91e32949c6f1edb88fafce21af41e6934ec8a353
Yeong Jun Joo <juyj7282@gmail.com>
2021-01-24 16:39:17 +0900

    [BE] feat:User API Exception 추가 - #60
    User API에서 나올 수 있는 Exception들을 추가했다.
    그리고 GlobalExceptionHanlder에서 Exception들을 처리해주고있다.

--- BE/user/src/main/java/com/everyportfolio/user/exception/GlobalExceptionHandler.java
+++ BE/user/src/main/java/com/everyportfolio/user/exception/GlobalExceptionHandler.java
@@ -14,8 +14,8 @@ public class GlobalExceptionHandler {
     private final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(PasswordLengthNotAllowedException.class)
-    public ResponseEntity<HashMap<String, Object>> passwordLengthNotAllowedExcepton(PasswordLengthNotAllowedException e) {
-        log.info("password's length is not allowed, " + e.getMessage());
+    public ResponseEntity<HashMap<String, Object>> passwordLengthNotAllowedException(PasswordLengthNotAllowedException e) {
+        log.info("PasswordLengthNotAllowedException : " + e.getMessage());
 
         HashMap<String, Object> result = new HashMap<>();
 
@@ -24,4 +24,61 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(IdDuplicatedException.class)
+    public ResponseEntity<HashMap<String, Object>> idDuplicatedException (IdDuplicatedException e) {
+        log.info("IdDuplicationException : " + e.getMessage());
+
+        HashMap<String, Object> result = new HashMap<>();
+
+        result.put("message", "this id already existed");
+        result.put("status", 409);
+        return new ResponseEntity<>(result, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(EmailRegularExpressionNotMatchedException.class)
+    public ResponseEntity<HashMap<String, Object>> emailRegularExpressionNotMatchedException(EmailRegularExpressionNotMatchedException e) {
+        log.info("EmailRegularExpressionNotMatchedException : "+ e.getMessage());
+
+        HashMap<String, Object> result = new HashMap<>();
+
+        result.put("message", "id must be same as email format");
+        result.put("status", 400);
+        return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(LoginDeniedException.class)
+    public ResponseEntity<HashMap<String, Object>> loginDeniedException(LoginDeniedException e) {
+        log.info("LoginDeniedException : " + e.getMessage());
+
+        HashMap<String, Object> result = new HashMap<>();
+
+        result.put("message", "id don't exist or password is not correct");
+        result.put("status", 401);
+
+        return new ResponseEntity<>(result, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(RefreshTokenDifferentException.class)
+    public ResponseEntity<HashMap<String, Object>> refreshTokenDiffrentException(RefreshTokenDifferentException e) {
+        log.info("RefreshTokenDifferentException : " + e.getMessage());
+
+        HashMap<String, Object> result = new HashMap<>();
+
+        result.put("message", "refresh token is not equal to server side refresh token");
+        result.put("status", 401);
+
+        return new ResponseEntity<>(result, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(EmailAuthenticationFailedException.class)
+    public ResponseEntity<HashMap<String, Object>> emailAuthenticationFailedException(EmailAuthenticationFailedException e) {
+        log.info("EmailAuthenticationFailedException : " + e.getMessage());
+
+        HashMap<String, Object> result = new HashMap<>();
+
+        result.put("message", "email token is already expired or not equal to server side");
+        result.put("status", 401);
+
+        return new ResponseEntity<>(result, HttpStatus.UNAUTHORIZED);
+    }
 }
