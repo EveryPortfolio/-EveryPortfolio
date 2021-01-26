bdd938bd0cdef2f19210872178a809cbb5a489aa
JinGu <xortm854@naver.com>
2021-01-26 23:04:38 +0900

    [FE]add: 불필요한 코드제거 및 redux 비동기코드-#32
    출력하는 불필요한 코드를 제거하고 redux 비동기를 위한 코드를
    수정하였습니다. 비동기에 대해 아직 이해가 부족해서 좀 더 학습해야합니다.

--- FE/src/container/LoginContainer/RightContainer.tsx
+++ FE/src/container/LoginContainer/RightContainer.tsx
@@ -26,7 +26,10 @@ export const RightContainer = (): JSX.Element => {
     e.preventDefault();
     console.log('click button');
     const params = { id: email, password };
-    dispatch(requestLogin(params));
+    requestLogin(params).then((result) => dispatch(result));
+    // new Promise((resolve, reject) => {
+    //   resolve(dispatch(requestLogin(params)));
+    // }).then((result) => console.log('result:', result));
     setEmail('');
     setPassword('');
     // router.push('/');
