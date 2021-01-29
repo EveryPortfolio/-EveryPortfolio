a0a66b0c8164255baddfb704790996c730cdf97a
JinGu <xortm854@naver.com>
2021-01-29 15:28:11 +0900

    [FE]refactor: 기존코드를 _app.tsx로 이동-#93
    기존에 index에서 적용하던 스토어와 스타일은 index 페이지 안에서만 적용
    되었기 때문에 모든 페이지에 적용시켜주기 위해 _app.tsx로 코드를
    이동하였습니다.

--- FE/src/pages/index.tsx
+++ FE/src/pages/index.tsx
@@ -1,20 +1,11 @@
 import * as React from 'react';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 import LoginComponent from '../components/LoginComponent/LoginComponent';
-import { GlobalStyle } from '../styles/GlobalStyle';
-import { store, persistor } from '../store/store';
 
 const Index: React.FunctionComponent = () => {
   return (
-    <div>
-      <Provider store={store}>
-        <PersistGate persistor={persistor}>
-          <GlobalStyle />
-          <LoginComponent />
-        </PersistGate>
-      </Provider>
-    </div>
+    <>
+      <LoginComponent />
+    </>
   );
 };
 
