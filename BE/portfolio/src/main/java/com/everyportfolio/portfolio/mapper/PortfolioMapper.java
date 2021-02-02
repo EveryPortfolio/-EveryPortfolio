5ce0b18c0ba316f91392602b768aeab5b7042822
Yeong Jun Joo <juyj7282@gmail.com>
2021-02-02 16:06:51 +0900

    [BE] add: update content용 SQL과 Mapper 추가 - #80
    update content용 SQL을 PortfolioMapper.xml에 추가하고
    그에 맞는 메소드를 PortfolioMapper에 추가함

--- BE/portfolio/src/main/java/com/everyportfolio/portfolio/mapper/PortfolioMapper.java
+++ BE/portfolio/src/main/java/com/everyportfolio/portfolio/mapper/PortfolioMapper.java
@@ -8,4 +8,6 @@ public interface PortfolioMapper {
     void insertPortfolio(@Param("id") String id, @Param("title") String title, @Param("content") String content, @Param("templateType") int templateType);
     String selectPortfolioUserIdByTableId(@Param("tableId") int tableId);
     void updatePortfolioTitle(@Param("tableId") int tableId, @Param("title") String title);
+    void updatePortfolioContent(@Param("tableId") int tableId, @Param("templateType") int templateType, @Param("content") String content);
+
 }
