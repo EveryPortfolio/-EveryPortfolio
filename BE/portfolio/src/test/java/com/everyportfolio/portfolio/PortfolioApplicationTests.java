f364243ab4438cf6ea615dacad0d1a448e075ca6
Yeong Jun Joo <juyj7282@gmail.com>
2021-02-02 15:42:48 +0900

    [BE] test: update title용 test code - #80
    update title용 test code, accesstoken은 대충 만들어서 씀

--- BE/portfolio/src/test/java/com/everyportfolio/portfolio/PortfolioApplicationTests.java
+++ BE/portfolio/src/test/java/com/everyportfolio/portfolio/PortfolioApplicationTests.java
@@ -1,13 +1,40 @@
 package com.everyportfolio.portfolio;
 
+import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.HashMap;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@AutoConfigureMockMvc
 @SpringBootTest
 class PortfolioApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void checkUpdateTitle() {
+		String accessToken = "{ \"id\": \"juyj7282@gmail.com\", \"auth\" : \"USER\" }";
+		HashMap<String, Object> params = new HashMap<>();
+
+		params.put("tableId", "10");
+		params.put("title", "changeTitle!!");
+
+		try {
+			mockMvc.perform(MockMvcRequestBuilders.put("/update/title").contentType("application/json").header("access-token", accessToken).content((new Gson()).toJson(params))).andDo(print());
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
