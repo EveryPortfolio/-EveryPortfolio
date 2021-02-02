d44b3a286054afc89bc3f110debdb56b78464ac4
Yeong Jun Joo <juyj7282@gmail.com>
2021-02-02 14:43:33 +0900

    [BE] add: portfolio mapper(SQL 연동) 제작 - #77
    Portfolio용 Mapper 만들어서 SQL과 연동. Create용 Insert문은 만들어둠

---
+++ BE/portfolio/src/main/java/com/everyportfolio/portfolio/mapper/PortfolioMapper.java
@@ -0,0 +1,9 @@
+package com.everyportfolio.portfolio.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface PortfolioMapper {
+    void insertPortfolio(@Param("id") String id, @Param("title") String title, @Param("content") String content, @Param("templateType") int templateType);
+}
