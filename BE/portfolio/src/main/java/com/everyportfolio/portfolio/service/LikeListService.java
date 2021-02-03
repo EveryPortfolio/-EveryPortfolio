ddf38e9f10b81e112e22caec81ce1c9320f0bc40
Yeong Jun Joo <juyj7282@gmail.com>
2021-02-03 16:25:19 +0900

    [BE] add: Like Switch 기능 해주는 Service Method 추가 - #106
    존재하면 delete 아니면 insert 해주는 service method 추가
    검색 + delete or insert 니 Transcational을 써서 Thread-safe하게
    동작하도록 해줘야 한다.

--- BE/portfolio/src/main/java/com/everyportfolio/portfolio/service/LikeListService.java
+++ BE/portfolio/src/main/java/com/everyportfolio/portfolio/service/LikeListService.java
@@ -4,6 +4,8 @@ import com.everyportfolio.portfolio.mapper.LikeListMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
 @AllArgsConstructor
 public class LikeListService {
@@ -11,5 +13,15 @@ public class LikeListService {
 
     public int selectLikeCountByTableId(int tableId) {
         return likeListMapper.selectLikeCountByTableId(tableId);
+    }
+
+    @Transactional
+    public void switchLikeByTableId(int tableId, String userId) {
+        if(likeListMapper.selectLikeCountByTableIdAndUserId(tableId, userId) == 1) {
+            likeListMapper.deleteLikeListByTableIdAndUserId(tableId, userId);
+        }
+        else {
+            likeListMapper.insertLikeList(tableId, userId);
+        }
     }
 }
