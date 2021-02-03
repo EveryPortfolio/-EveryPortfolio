3a2a6c3fa6ba18a854c94f20d80c6a17c5e7557f
Yeong Jun Joo <juyj7282@gmail.com>
2021-02-03 16:24:17 +0900

    [BE] add: portfolio dto 및 SQL과 Mapper 추가 - #106
    switch on/off를 위해서 insert, delete 추가
    그리고 /like를 위해서 DTO 추가. DTO 내용엔 TableId만 있음

---
+++ BE/portfolio/src/main/java/com/everyportfolio/portfolio/DTO/PortfolioLikeDTO.java
@@ -0,0 +1,12 @@
+package com.everyportfolio.portfolio.DTO;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PortfolioLikeDTO {
+    private int tableId;
+}
