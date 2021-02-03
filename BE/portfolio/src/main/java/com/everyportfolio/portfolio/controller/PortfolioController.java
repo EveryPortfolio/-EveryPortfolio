cd7480fe74830c0a20bdc4773d6ecaaa3827649d
Yeong Jun Joo <juyj7282@gmail.com>
2021-02-03 15:19:49 +0900

    [BE] add: portfolio view를 위한 controller 작성 - #82
    controller에서 PortfolioService에 요청해서 Portfolio 가져오고
    LikeListService에서 Like 갯수 가져와서 return
    중간에 tableId가 Double 형으로 변환되길래 Int형으로 바꿔줌

--- BE/portfolio/src/main/java/com/everyportfolio/portfolio/controller/PortfolioController.java
+++ BE/portfolio/src/main/java/com/everyportfolio/portfolio/controller/PortfolioController.java
@@ -1,8 +1,10 @@
 package com.everyportfolio.portfolio.controller;
 
 import com.everyportfolio.portfolio.DTO.*;
+import com.everyportfolio.portfolio.service.LikeListService;
 import com.everyportfolio.portfolio.service.PortfolioService;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +21,7 @@ public class PortfolioController {
     private final Logger log = LoggerFactory.getLogger(PortfolioController.class);
     private Gson gson;
     private PortfolioService portfolioService;
+    private LikeListService likeListService;
 
     @PostMapping("/create")
     public ResponseEntity<HashMap<String, Object>> createPortfolio(@RequestBody PortfolioDTO portfolio, @RequestHeader("access-token") String accessToken) {
@@ -73,6 +76,20 @@ public class PortfolioController {
         HashMap<String, Object> result = new HashMap<>();
         result.put("status", 200);
         result.put("message", "OK");
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping("/view")
+    public ResponseEntity<HashMap<String, Object>> viewPortfolio(@RequestParam("tableId") int tableId) {
+
+        String content = gson.toJson(portfolioService.selectPortfolioByTableId(tableId));
+        HashMap<String, Object> result = gson.fromJson(content, new TypeToken<HashMap<String, Object>>(){}.getType());
+
+        result.put("status", 200);
+        result.put("message", "OK");
+        result.put("tableId", (int)Double.parseDouble(result.get("tableId").toString()));
+        result.put("likeCount", likeListService.selectLikeCountByTableId(tableId));
 
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
