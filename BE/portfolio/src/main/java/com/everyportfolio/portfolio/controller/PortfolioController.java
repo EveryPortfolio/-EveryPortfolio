205dd5bb7d6fc9734d8b6892750efefb36eff8c2
Yeong Jun Joo <juyj7282@gmail.com>
2021-02-09 16:27:17 +0900

    [BE] add: controller에 Exception 추가 - #85
    Exception 상황들을 Controller에 추가

--- BE/portfolio/src/main/java/com/everyportfolio/portfolio/controller/PortfolioController.java
+++ BE/portfolio/src/main/java/com/everyportfolio/portfolio/controller/PortfolioController.java
@@ -1,19 +1,21 @@
 package com.everyportfolio.portfolio.controller;
 
 import com.everyportfolio.portfolio.DTO.*;
+import com.everyportfolio.portfolio.exception.*;
+import com.everyportfolio.portfolio.model.Portfolio;
 import com.everyportfolio.portfolio.service.LikeListService;
 import com.everyportfolio.portfolio.service.PortfolioService;
+import com.everyportfolio.portfolio.utility.RegularExpressionUtility;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import com.sun.istack.Nullable;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import javax.xml.ws.Response;
 import java.util.HashMap;
 
 @RestController
@@ -23,10 +25,17 @@ public class PortfolioController {
     private Gson gson;
     private PortfolioService portfolioService;
     private LikeListService likeListService;
+    private RegularExpressionUtility regularExpressionUtility;
 
     @PostMapping("/create")
-    public ResponseEntity<HashMap<String, Object>> createPortfolio(@RequestBody PortfolioDTO portfolio, @RequestHeader("access-token") String accessToken) {
+    public ResponseEntity<HashMap<String, Object>> createPortfolio(@RequestBody PortfolioDTO portfolio, @RequestHeader("access-token") String accessToken) throws TooLongTitleException, TooLongThumbnailURLException{
         String id = gson.fromJson(accessToken, AccessTokenDTO.class).getId();
+
+        if(portfolio.getTitle() != null && portfolio.getTitle().length() > 50)
+            throw new TooLongTitleException(id + "'s portfolio title request is too long");
+
+        if(portfolio.getThumbnailURL() != null && portfolio.getThumbnailURL().length() > 2048)
+            throw new TooLongThumbnailURLException(id + "'s portfolio thumbnail URL is too long");
 
         portfolioService.createPortfolio(id, portfolio.getTitle(), portfolio.getContent(), portfolio.getTemplateType(), portfolio.getThumbnailURL());
 
@@ -39,6 +48,10 @@ public class PortfolioController {
     @PutMapping("/update/title")
     public ResponseEntity<HashMap<String, Object>> updatePortfolioTitle(@RequestBody PortfolioTitleDTO title, @RequestHeader("access-token") String accessToken) throws Exception{
         String id = gson.fromJson(accessToken, AccessTokenDTO.class).getId();
+
+        if(title.getTitle() != null && title.getTitle().length() > 50)
+            throw new TooLongTitleException(id + "'s portfolio title request is too long");
+
         if(!portfolioService.compareUserIdWithCreator(id, title.getTableId()))
             throw new Exception();
 
@@ -55,7 +68,7 @@ public class PortfolioController {
     public ResponseEntity<HashMap<String, Object>> updatePortfolioContent(@RequestBody PortfolioContentDTO content, @RequestHeader("access-token") String accessToken) throws Exception{
         String id = gson.fromJson(accessToken, AccessTokenDTO.class).getId();
         if(!portfolioService.compareUserIdWithCreator(id, content.getTableId()))
-            throw new Exception();
+            throw new NotEqualToWriterException(id + "'s request is not equal to " + content.getTableId() + "'s writer");
 
         portfolioService.updatePortfolioContent(content.getTableId(), content.getTemplateType(), content.getContent());
         HashMap<String, Object> result = new HashMap<>();
@@ -70,7 +83,7 @@ public class PortfolioController {
         String id = gson.fromJson(accessToken, AccessTokenDTO.class).getId();
 
         if(!portfolioService.compareUserIdWithCreator(id, delete.getTableId()))
-            throw new Exception();
+            throw new NotEqualToWriterException(id + "'s request is not equal to " + delete.getTableId() + "'s writer");
 
         portfolioService.deletePortfolio(delete.getTableId());
 
@@ -82,7 +95,9 @@ public class PortfolioController {
     }
 
     @GetMapping("/view")
-    public ResponseEntity<HashMap<String, Object>> viewPortfolio(@RequestParam("tableId") int tableId) {
+    public ResponseEntity<HashMap<String, Object>> viewPortfolio(@RequestParam("tableId") int tableId) throws Exception{
+        if(!portfolioService.existPortfolioByTableId(tableId))
+            throw new NotExistPortfolioException(tableId + " doesn't exist");
 
         String content = gson.toJson(portfolioService.selectPortfolioByTableId(tableId));
         HashMap<String, Object> result = gson.fromJson(content, new TypeToken<HashMap<String, Object>>(){}.getType());
@@ -99,7 +114,7 @@ public class PortfolioController {
     public ResponseEntity<HashMap<String, Object>> switchLikeList(@RequestBody PortfolioLikeDTO like, @RequestHeader("access-token") String accessToken) throws Exception{
 
         if(!portfolioService.existPortfolioByTableId(like.getTableId()))
-            throw new Exception();
+            throw new NotExistPortfolioException(like.getTableId() + " doesn't exist");
 
         String id = gson.fromJson(accessToken, AccessTokenDTO.class).getId();
 
@@ -115,7 +130,7 @@ public class PortfolioController {
     @GetMapping("/like")
     public ResponseEntity<HashMap<String, Object>> getLikeList(@RequestParam("tableId") int tableId, @RequestHeader("access-token") String accessToken) throws Exception{
         if(!portfolioService.existPortfolioByTableId(tableId))
-            throw new Exception();
+            throw new NotExistPortfolioException(tableId + " doesn't exist");
 
         String id = gson.fromJson(accessToken, AccessTokenDTO.class).getId();
 
@@ -137,8 +152,11 @@ public class PortfolioController {
                                                                        @RequestParam(value = "maxLikeCount", required = false) Integer maxLikeCount,
                                                                        @RequestParam(value = "userId", required = false) String userId,
                                                                        @RequestParam(value = "title", required = false) String title) throws Exception{
-        if(title != null)
-            title = title.trim();
+        if(!regularExpressionUtility.emailPatternMatch(userId))
+            throw new EmailRegularExpressionNotMatchedException(userId + " is not matched by email format");
+
+        if(title != null && title.length() > 50)
+            throw new TooLongTitleException("Searcher's title request is too long");
 
         HashMap<String, Object> result = new HashMap<>();
 
@@ -146,10 +164,12 @@ public class PortfolioController {
             result.put("portfolioList", portfolioService.selectPortfolioListByLatest(maxTableId, userId, title));
         }else if(type == 2) { // 좋아요 순
             if(maxLikeCount == null)
-                throw new Exception();
+                throw new NotExistLikeCountException("maxLikeCount cannot be null while like search");
 
             result.put("portfolioList", portfolioService.selectPortfolioListByLikeCount(maxTableId, maxLikeCount, userId, title));
         }
+        else
+            throw new NotVaildTypeException(type + " is not allowed in search method");
 
         result.put("status", 200);
         result.put("message", "OK");
